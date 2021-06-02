084bfd1a050715ce633c7f974d9490f6ed99be74
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-02 16:17:45 +0200

    Added Activities for All Screens

---
+++ app/src/main/java/com/SleeplessStudios/modernpocket/CollectionsMain.java
@@ -0,0 +1,14 @@
+package com.SleeplessStudios.modernpocket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollectionsMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collections_main);
+    }
+}

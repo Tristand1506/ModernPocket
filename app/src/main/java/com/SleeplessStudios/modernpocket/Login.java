df841e291437a183c6cf44399088f011f9cc07e6
Tristan van Wyk <tristanvwyk@gmail.com>
2021-06-03 23:35:02 +0200

    did a vertical layout thing for the main screen

--- app/src/main/java/com/SleeplessStudios/modernpocket/Login.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/Login.java
@@ -29,7 +29,7 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-                openMainScreen();
+                LogInAccount();
             }
         });
     }

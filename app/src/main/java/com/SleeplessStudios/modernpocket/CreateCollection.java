c7b9713195d2e32cd7323f4531ac20c4a70c7cef
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-03 18:33:29 +0200

    Spinner aaaaaaaaaaaaaa

--- app/src/main/java/com/SleeplessStudios/modernpocket/CreateCollection.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/CreateCollection.java
@@ -3,12 +3,26 @@ package com.SleeplessStudios.modernpocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateCollection extends AppCompatActivity {
+    private Spinner spinnerChooseItemType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_collection);
+
+        spinnerChooseItemType = findViewById(R.id.chooseitemtype_spinner);
+        //array
+        String[] itemTypeList = getResources().getStringArray(R.array.item_types);
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, itemTypeList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerChooseItemType.setAdapter(adapter);
+
     }
 }

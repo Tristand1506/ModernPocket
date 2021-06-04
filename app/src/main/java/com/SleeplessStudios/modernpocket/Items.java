4070730638884d6812e6dd7f2198a0a9e37299a9
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-04 13:12:30 +0200

    Create Collection UI and some other stuff

--- app/src/main/java/com/SleeplessStudios/modernpocket/Items.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/Items.java
@@ -2,13 +2,33 @@ package com.SleeplessStudios.modernpocket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Items extends AppCompatActivity {
+    private ImageButton createItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_items);
+
+        createItem = (ImageButton) findViewById(R.id.create_item_btn);
+        createItem.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openItemCreation();
+            }
+        });
+    }
+
+    public void openItemCreation()
+    {
+        Intent intent = new Intent(this, CreateItem.class);
+        startActivity(intent);
     }
 }

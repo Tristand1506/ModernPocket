4bc2cfd5f45bd39a065c201ae82c5c98af082abd
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-05 00:34:19 +0200

    Fixed Some UI Stuff

--- app/src/main/java/com/SleeplessStudios/modernpocket/CreateItemType.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/CreateItemType.java
@@ -3,12 +3,21 @@ package com.SleeplessStudios.modernpocket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class CreateItemType extends AppCompatActivity {
+    private ImageButton save;
+    private ImageButton discard;
+    private EditText itemName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_item_type);
+
+        save = (ImageButton) findViewById(R.id.save_item_type_btn);
+        discard = (ImageButton) findViewById(R.id.x_createitem_btn);
+        itemName = (EditText) findViewById(R.id.item_name_txt);
     }
 }

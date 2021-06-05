e1ee74f4811c434905ff9d13c64c35bc9c4a2cc2
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-05 20:32:10 +0200

    scuffed nav bar done

--- app/src/main/java/com/SleeplessStudios/modernpocket/EditProfile.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/EditProfile.java
@@ -1,16 +1,21 @@
 package com.SleeplessStudios.modernpocket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 import com.google.android.material.navigation.NavigationView;
 
-public class EditProfile extends AppCompatActivity{
+public class EditProfile extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private ImageButton avatar;
     private ImageButton sidebar;
     private EditText email;
@@ -30,13 +35,90 @@ public class EditProfile extends AppCompatActivity{
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, R.string.sidebar_open, R.string.sidebar_close);
         drawer.addDrawerListener(toggle);
         NavigationView navView = findViewById(R.id.sidebar_view);
+        navView.setNavigationItemSelectedListener(this);
 
         avatar = (ImageButton) findViewById(R.id.profile_avatar_btn);
         sidebar = (ImageButton) findViewById(R.id.burger_bar_profile_btn);
+        sidebar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openSidebar();
+            }
+        });
 
         email = (EditText) findViewById(R.id.edit_email_txt);
         phone = (EditText) findViewById(R.id.edit_phone_txt);
         gender = (EditText) findViewById(R.id.edit_gender_txt);
         DOB = (EditText) findViewById(R.id.edit_DOB_txt);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.sidebar_profile:
+                openProfile();
+                break;
+            case R.id.sidebar_fav:
+                openFavourites();
+                break;
+            case R.id.sidebar_lent:
+                openLent();
+                break;
+            case R.id.sidebar_settings:
+                openSettings();
+                break;
+            case R.id.sidebar_rate:
+                openRate();
+                break;
+            case R.id.sidebar_help:
+                openHelp();
+                break;
+            case R.id.sidebar_logout:
+                Logout();
+                break;
+        }
+        return true;
+    }
+    public void openSidebar()
+    {
+        drawer.openDrawer(GravityCompat.END);
+    }
+
+    public void openProfile()
+    {
+        Intent intent = new Intent(this, EditProfile.class);
+        startActivity(intent);
+    }
+    public void openFavourites()
+    {
+        Intent intent = new Intent(this, Favourites.class);
+        startActivity(intent);
+    }
+    public void openLent()
+    {
+        Intent intent = new Intent(this, LentItems.class);
+        startActivity(intent);
+    }
+    public void openSettings()
+    {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
+    }
+
+    public void Logout()
+    {
+
+    }
+    //-----------------------TO DO--------------------------------------
+    public void openRate()
+    {
+
+    }
+    public void openHelp()
+    {
+
     }
 }

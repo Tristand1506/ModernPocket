bfa4aea0bddfa1b64dce529c2b5b44d3a0301079
Ilse-May Brink <18023892@vegaconnect.co.za>
2021-06-07 10:59:17 +0200

    scrollable item creation v3

--- app/src/main/java/com/SleeplessStudios/modernpocket/CollectionsMain.java
+++ app/src/main/java/com/SleeplessStudios/modernpocket/CollectionsMain.java
@@ -154,7 +154,8 @@ public class CollectionsMain extends AppCompatActivity implements NavigationView
 
     public void Logout()
     {
-
+        Intent intent = new Intent(this, LandingPage.class);
+        startActivity(intent);
     }
     //-----------------------TO DO--------------------------------------
     public void openHelp()
